c14c920790b7bba04d10af5d8d2e723bb8da0784
MrSon <sonk9d@gmail.com>
2022-11-19 10:28:34 +0700

    sua file

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Depends
 import models
 from database import engine
-from routers import auth, todos, users, address
+from routers import auth, todos
 from company import companyapis, dependencies
 from starlette.staticfiles import StaticFiles
 
@@ -12,10 +12,10 @@ app.mount('/static', StaticFiles(directory='static'), name='static')
 app.include_router(auth.router)
 app.include_router(todos.router)
 app.include_router(users.router)
-app.include_router(address.router)
-app.include_router(companyapis.router,
-                   prefix='/companyapis',
-                   tags=['companyapis'],
-                   dependencies=[Depends(dependencies.get_token_header)],
-                   responses={418: {'description': 'Internal Use Only'}}
-                   )
+# app.include_router(address.router)
+# app.include_router(companyapis.router,
+#                    prefix='/companyapis',
+#                    tags=['companyapis'],
+#                    dependencies=[Depends(dependencies.get_token_header)],
+#                    responses={418: {'description': 'Internal Use Only'}}
+#                    )
--- TodoApp/models.py
+++ TodoApp/models.py
@@ -32,17 +32,16 @@ class Todos(Base):
 
     owner = relationship("Users", back_populates="todos")
 
-
-class Address(Base):
-    __tablename__ = 'address'
-
-    id = Column(Integer, primary_key=True, index=True)
-    address1 = Column(String)
-    address2 = Column(String)
-    city = Column(String)
-    state = Column(String)
-    country = Column(String)
-    postalcode = Column(String)
-    apt_num = Column(Integer)
-
-    user_address = relationship('Users', back_populates="address")
+# class Address(Base):
+#     __tablename__ = 'address'
+#
+#     id = Column(Integer, primary_key=True, index=True)
+#     address1 = Column(String)
+#     address2 = Column(String)
+#     city = Column(String)
+#     state = Column(String)
+#     country = Column(String)
+#     postalcode = Column(String)
+#     apt_num = Column(Integer)
+#
+#     user_address = relationship('Users', back_populates="address")
