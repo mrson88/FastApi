6028423e9458799b1795c796988bd2a85bbd1b7d
MrSon <sonk9d@gmail.com>
2022-11-21 16:17:27 +0700

    update git fastapi routing4

--- FastAPI-Routing/TodoApp/database.py
+++ FastAPI-Routing/TodoApp/database.py
@@ -7,9 +7,9 @@ from sqlalchemy.ext.declarative import declarative_base
 # MYSQL Series
 # SQLALCHEMY_DATABASE_URL = "postgresql://postgres:daovanson88@localhost/TodoApplicationDatabase"
 SQLALCHEMY_DATABASE_URL = "postgresql://ioipbvjh:gh55DaBH3Ikcl3nRdWfOK4ILYIlaHoHD@satao.db.elephantsql.com/ioipbvjh"
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+# )
 
 # MYSQL Series
 # engine = create_engine(
