c57d2676f7b2a700ceb01dc85f2670c6a2b6ec23
MrSon <sonk9d@gmail.com>
2023-01-21 06:52:01 +0700

    creat newserver loto-1

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
 
 # MYSQL Series
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:daovanson88@localhost/mrsonbd"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:daovanson88@localhost/mrsondb"
 
 # engine = create_engine(
 #     SQLALCHEMY_DATABASE_URL
