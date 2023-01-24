78a71cc8049e461175a012d9ee51741a8d6418de
MrSon <sonk9d@gmail.com>
2023-01-24 07:38:22 +0700

    creat newserver loto-10-8

--- TodoApp/routers/xsmb.py
+++ TodoApp/routers/xsmb.py
@@ -73,7 +73,7 @@ async def read_kqxs5p(day: str,
                       db: Session = Depends(get_db)):
     xs5p_model = db.query(models.ResultFiveMinute.result) \
         .filter(models.ResultFiveMinute.day == day) \
-        .first()
+        .last()
     if xs5p_model is not None:
         return xs5p_model
     raise http_exception()
