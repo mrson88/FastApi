4994ba878aa38056cdcb272057acceec4787553b
MrSon <sonk9d@gmail.com>
2023-01-24 15:22:13 +0700

    creat newserver loto-10-9

--- TodoApp/check_data/check_data_daily.py
+++ TodoApp/check_data/check_data_daily.py
@@ -105,7 +105,7 @@ class CheckDataDaily:
                     if bb in result_calculate:
                         x += 1
                         print('bb=', bb)
-                # print('x=', x)
+                print('x=', x)
                 if x > 0:
                     self.cur.execute(f"update payment set data_money=data_money+{x}*%s where owner_id = %s",
                                      (xien_5p, id_owner,))
