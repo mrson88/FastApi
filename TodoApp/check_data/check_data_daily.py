3fae4588d4db38904f9df39738dba57d9019eb1b
MrSon <sonk9d@gmail.com>
2023-01-28 10:22:15 +0700

    creat newserver loto-10-29

--- TodoApp/check_data/check_data_daily.py
+++ TodoApp/check_data/check_data_daily.py
@@ -87,9 +87,9 @@ class CheckDataDaily:
             (date_today_strf(), 'xs_5p', id_owner, False))
         result_data = self.cur.fetchall()
         print('result_data_five_minute=', result_data)
-        price = result_data[0][1]
 
         if result:
+            price = result_data[0][1]
             result_calculate = []
             for i in range(len(result[0])):
                 result_calculate.append(result[0][i])
