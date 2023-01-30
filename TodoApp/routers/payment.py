0f50a6834cc9cafa5714d55b6206169fa008a8ac
MrSon <sonk9d@gmail.com>
2023-01-30 23:03:52 +0700

    creat newserver loto-10-46

--- TodoApp/routers/payment.py
+++ TodoApp/routers/payment.py
@@ -139,6 +139,7 @@ async def add_payment(payment_id: int,
 
     payment_model.date = payment.date
     payment_model.time = payment.time
+    payment_model.data_type = payment.data_type
     if float(payment.data_money) > 0 and payment_model.active:
         if not payment_model.daily_pay:
             payment_model.data_money = payment_model.data_money + 3000000
