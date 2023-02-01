57391228c197070a4c2cf36562e5afa6d0ad5b30
MrSon <sonk9d@gmail.com>
2023-02-02 03:24:19 +0700

    creat newserver loto-10-47

--- TodoApp/check_data/check_data_daily.py
+++ TodoApp/check_data/check_data_daily.py
@@ -118,7 +118,7 @@ class CheckDataDaily:
                 # print('x=', x)
                 if x > 0:
                     self.cur.execute(
-                        f"update payment set data_money=data_money+{x * float(price)}*%s where owner_id = %s",
+                        f"update payment set data_money=data_money+{x}*%s where owner_id = %s",
                         (win_xien_5p, id_owner,))
                     # self.connection.commit()
                     print('x=', x)
@@ -126,7 +126,7 @@ class CheckDataDaily:
                     self.cur.execute(query,
                                      (
                                          date_today_strf(), time_today(),
-                                         f'+{float(round((win_xien_5p * x * float(price)), 2))}',
+                                         f'+{float(round((win_xien_5p * x), 2))}',
                                          'win_xs_5p',
                                          id_owner,))
                     self.connection.commit()
