ee8d8e6239ac62b7d649b8e16f454065b2352be9
MrSon <sonk9d@gmail.com>
2023-02-04 00:35:41 +0700

    creat newserver loto-10-49

--- TodoApp/routers/payment.py
+++ TodoApp/routers/payment.py
@@ -114,7 +114,7 @@ async def minus_payment(payment_id: int,
             payment_model.data_money -= float(payment.data_money) * 27000
             # print('3')
         elif payment.data_type in ['L2', 'L3', 'L4']:
-            payment_model.data_cost -= str(float(payment.data_money) * 20000)
+            payment_model.data_money -= float(payment.data_money) * 20000
 
         elif payment.data_type in ['D2', 'D3', 'D4', 'D_2p', 'D3C_2p', 'D4C_2p']:
             # print('4')
@@ -197,17 +197,17 @@ async def create_payment_history(payment_his: PaymentHistory,
     if float(payment_his.data_money) > 0:
         if payment_his.data_type in ['x2', 'x3', 'x4', 'x2_2p', 'x3_2p', 'x4_2p']:
             # print('1')
-            payment_his_model.data_money = float(payment_his.data_money) * 10000
+            payment_his_model.data_money = payment_his.data_money * 10000
         elif payment_his.data_type in ['L_2p', 'L1', 'L3C_2p', 'L4C_2p']:
             # print('2')
-            payment_his_model.data_money = float(payment_his.data_money) * 27000
+            payment_his_model.data_money = payment_his.data_money * 27000
             # print('3')
         elif payment_his.data_type in ['L2', 'L3', 'L4']:
-            payment_his_model.data_cost = str(float(payment_his.data_money) * 20000)
+            payment_his_model.data_money = payment_his.data_money * 20000
 
         elif payment_his.data_type in ['D2', 'D3', 'D4', 'D_2p', 'D3C_2p', 'D4C_2p']:
             # print('4')
-            payment_his_model.data_money = float(payment_his.data_money) * 1000
+            payment_his_model.data_money = payment_his.data_money * 1000
         else:
             raise http_exception()
 
