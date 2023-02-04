a7df42424bc5db4b7a065c1c0564ca5934312813
MrSon <sonk9d@gmail.com>
2023-02-05 03:16:42 +0700

    creat newserver loto-10-49

--- TodoApp/check_data/check_data_daily.py
+++ TodoApp/check_data/check_data_daily.py
@@ -87,6 +87,16 @@ class CheckDataDaily:
 
     def check_data_five_minute(self, id_owner, data_type):
         win_xien_5p = 99 / 27
+        win_xien_5p = {
+            'x2': 17,
+            'x3': 74,
+            'x4': 251,
+            'L2': 99 / 27,
+            'L3': 97.23 / 2,
+            'L4': 450,
+            'D3': 972.3,
+            'D4': 9000
+        }
         # data_type=['x2', 'x3', 'x4','L2','L3', 'L4','D2', 'D3', 'D4']
         # print('check data')
         self.cur.execute("select result from result_five_minute where day = %s order by id desc limit 1",
@@ -137,8 +147,8 @@ class CheckDataDaily:
                     self.cur.execute(query,
                                      (
                                          date_today_strf(), time_today(),
-                                         f'+{float(round((win_xien_5p * x * float(price)), 2))}',
-                                         'win_L_2p',
+                                         f'+{float(round((win_xien_5p[data_type] * x * float(price)), 2))}',
+                                         data_type,
                                          id_owner, result_his))
                     self.connection.commit()
 
