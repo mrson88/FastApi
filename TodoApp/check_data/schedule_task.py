db56e17e61b169d78ea8269f61f90b56f9b88bff
MrSon <sonk9d@gmail.com>
2023-02-04 21:42:37 +0700

    creat newserver loto-10-49

--- TodoApp/check_data/schedule_task.py
+++ TodoApp/check_data/schedule_task.py
@@ -46,7 +46,8 @@ async def task_five_minute():
         local_time = time.localtime(seconds)
 
         await asyncio.sleep(0.1)
-        if int(local_time.tm_min) % 2 == 0 and int(local_time.tm_sec) < 10:
+        if int(local_time.tm_min) % 2 == 0 and int(local_time.tm_sec) in [0, 2]:
+            is_check_5_minute = False
             if not is_check_5_minute:
                 # print("time: ", local_time.tm_sec)
                 save_data = PostgresNoDuplicates()
