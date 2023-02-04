fb4c6774484ea8e9c4fd2a26bc166351350cd0cb
MrSon <sonk9d@gmail.com>
2023-02-04 21:45:45 +0700

    creat newserver loto-10-49

--- TodoApp/check_data/schedule_task.py
+++ TodoApp/check_data/schedule_task.py
@@ -45,8 +45,8 @@ async def task_five_minute():
         seconds = time.time()
         local_time = time.localtime(seconds)
 
-        await asyncio.sleep(0.1)
-        if int(local_time.tm_min) % 2 == 0 and int(local_time.tm_sec) in [0, 2]:
+        await asyncio.sleep(0.6)
+        if int(local_time.tm_min) % 2 == 0 and (int(local_time.tm_sec) in [0, 2]):
             is_check_5_minute = False
             if not is_check_5_minute:
                 # print("time: ", local_time.tm_sec)
