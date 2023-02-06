7c7e992ac6429862a4b5fcc8ddad23b2e511b773
MrSon <sonk9d@gmail.com>
2023-02-06 07:30:07 +0700

    creat newserver loto-10-49

--- TodoApp/check_data/check_data_daily.py
+++ TodoApp/check_data/check_data_daily.py
@@ -106,7 +106,7 @@ class CheckDataDaily:
         # print('result=', result)
 
         self.cur.execute(
-            "select data,data_cost,data_type from xsmb where date = %s and xs_type=%s and owner_id = %s and is_check = %s",
+            "select data,data_cost_per,data_type from xsmb where date = %s and xs_type=%s and owner_id = %s and is_check = %s",
             (date_today_strf(), 'xs_2p', id_owner, False))
         result_data = self.cur.fetchall()
 
@@ -130,7 +130,7 @@ class CheckDataDaily:
                 x = 0
                 result_his = []
                 if data_type == data_type_list[3]:
-                    price = float(float(result_data[j][1]) / len(result_data[j]))
+                    price = float(result_data[j][1])
                     print('price=', price)
                     # print('type=', data_type)
                     aa = result_data[j][0][0]
@@ -144,7 +144,7 @@ class CheckDataDaily:
                                 result_his.append(bb)
                         # print('x=', x)
                 elif data_type in data_type_list[:3]:
-                    price = float(float(result_data[j][1]) / len(result_data[j]))
+                    price = float(result_data[j][1])
                     print('price=', price)
                     # print('type=', data_type)
                     aa = result_data[j][0]
@@ -159,7 +159,7 @@ class CheckDataDaily:
                         # print('x=', x)
                 else:
                     # print('type=', data_type)
-                    price = float(float(result_data[j][1]) / len(result_data[j]))
+                    price = float(result_data[j][1])
                     print('price=', price)
                     aa = result_data[j][0][0]
                     for k in range(len(aa)):
