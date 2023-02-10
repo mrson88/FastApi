a7802f8dbce853573b16c703f2e67d5340ac85b5
MrSon <sonk9d@gmail.com>
2023-02-10 11:08:16 +0700

    creat newserver loto-10-49

--- TodoApp/routers/payment.py
+++ TodoApp/routers/payment.py
@@ -230,7 +230,7 @@ async def read_payment_history_by_user(user: dict = Depends(get_current_user),
 
 
 @router.get("/payment_history_all")
-async def read_payment_history_all(data: List[DataInput], db: Session = Depends(get_db)):
+async def read_payment_history_all(db: Session = Depends(get_db)):
     list_payment_all = db.query(models.PaymentHistory) \
         .filter(models.PaymentHistory.data_type == 'x4') \
         .all()
