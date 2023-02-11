85ba44dd5be6fd5ff389dc4c05b942290fac718e
MrSon <sonk9d@gmail.com>
2023-02-11 12:02:08 +0700

    creat newserver loto-10-49

--- TodoApp/routers/payment.py
+++ TodoApp/routers/payment.py
@@ -230,7 +230,7 @@ async def read_payment_history_by_user(user: dict = Depends(get_current_user),
     return list_payment_id
 
 
-@router.get("/payment_history_all/{day}")
+@router.get("/payment_history_all")
 async def read_payment_history_all(day: int, db: Session = Depends(get_db)):
     data_type_list = ['x2', 'x3', 'x4', 'L2', 'L3', 'L4', 'D2', 'D3', 'D4']
     data_type_win_list = [('win_' + str(i)) for i in data_type_list]
