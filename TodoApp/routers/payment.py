3cf6b1e6ac2a66aaeded7e90b8af2c4c5d9526e2
MrSon <sonk9d@gmail.com>
2023-02-11 11:00:06 +0700

    creat newserver loto-10-49

--- TodoApp/routers/payment.py
+++ TodoApp/routers/payment.py
@@ -239,7 +239,7 @@ async def read_payment_history_all(db: Session = Depends(get_db)):
     # print(list_day)
     list_payment_all = db.query(func.sum(models.PaymentHistory.data_money)).filter(
         (str(models.PaymentHistory.date) == '11-02-2023'), (models.PaymentHistory.owner_id) == 1,
-                                                           float(models.PaymentHistory.data_type) > 0).scalar()
+                                                           (models.PaymentHistory.data_money) > 0).scalar()
 
     # list_payment_all = db.query(func.sum(text(f"payment_history.data_money"))).filter(
     #     text(f"payment_history.date IN:list_day"), text(f"table_name.data_type IN :data_type_list")).params(
