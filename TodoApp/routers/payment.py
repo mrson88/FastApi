26c582ca23f1352a9394e7546d5f50f76ce4508d
MrSon <sonk9d@gmail.com>
2023-02-12 10:55:26 +0700

    creat newserver loto-10-49

--- TodoApp/routers/payment.py
+++ TodoApp/routers/payment.py
@@ -265,7 +265,7 @@ async def read_payment_history_all(db: Session = Depends(get_db)):
         #
         # print([str(list_payment_all), str(list_win_all)])
         print(type(final_payment))
-        all_data.append([f"{k[2]} {k[3]}", str(final_payment)])
+        all_data.append([f"{k[0]} {k[0]}", str(final_payment)])
     # print(all_data)
     return all_data
 
