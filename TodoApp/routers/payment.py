b8298648d8dd505609ab69088d8c55e711dd2697
MrSon <sonk9d@gmail.com>
2023-02-12 11:16:49 +0700

    creat newserver loto-10-49

--- TodoApp/routers/payment.py
+++ TodoApp/routers/payment.py
@@ -234,9 +234,9 @@ async def read_payment_history_by_user(user: dict = Depends(get_current_user),
 async def read_payment_history_all(db: Session = Depends(get_db)):
     data_type_list = ['x2', 'x3', 'x4', 'L2', 'L3', 'L4', 'D2', 'D3', 'D4']
     data_type_win_list = [('win_' + str(i)) for i in data_type_list]
-    list_user_id = db.query(models.Users.id, models.Users.first_name, models.Users.last_name) \
+    list_user_id = db.query(models.Payment.id, models.Payment.first_name, models.Payment.last_name) \
         .all()
-    print(list_user_id)
+    # print(list_user_id)
 
     list_day = [str((datetime.now().date() - timedelta(days=i)).strftime("%d-%m-%Y")) for i in range(1)]
     # print(list_user_id)
@@ -265,7 +265,7 @@ async def read_payment_history_all(db: Session = Depends(get_db)):
             final_payment += float(list_payment_all) + float(list_win_all)
         #
         # print([str(list_payment_all), str(list_win_all)])
-        print(type(final_payment))
+        # print(type(final_payment))
         all_data.append([k[0], str(final_payment), f"{str(k[1])} {str(k[2])}"])
     # print(all_data)
     return all_data
