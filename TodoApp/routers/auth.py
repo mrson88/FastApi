107804af8bde1c3292f90fd3d47e3a0f7832292f
MrSon <sonk9d@gmail.com>
2023-02-18 11:33:34 +0700

    creat newserver loto

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -114,25 +114,13 @@ async def sign_up(request: SignUpRequest, db: Session = Depends(get_db)):
     for data in list_user_email:
         list_user_email_final.append(data[0])
     print(list_user_email_final)
-    # if check_data_username(create_user.username) and check_data_other(create_user.email) and check_data_other(
-    #         create_user.username) and check_data_other(create_user.last_name):
-    #     create_user_model.email = create_user.email
-    #     create_user_model.username = create_user.username
-    #     create_user_model.first_name = create_user.first_name
-    #     create_user_model.last_name = create_user.last_name
-    #     hash_password = get_password_hash(create_user.password)
-    #     create_user_model.hashed_password = hash_password
-    #     create_user_model.is_active = True
-    #     db.add(create_user_model)
-    #     db.commit()
-    # else:
-    #     raise token_exception()
     if request.email in list_user_email_final:
         raise http_exception()
-    otp_codes[request.email] = otp_code
-    if send_otp_email(request.email, otp_code):
-        return {"message": "OTP code sent to email address."}
-    raise http_exception()
+    else:
+        otp_codes[request.email] = otp_code
+        if send_otp_email(request.email, otp_code):
+            return {"message": "OTP code sent to email address."}
+        raise http_exception()
 
 
 @router.post("/verify")
