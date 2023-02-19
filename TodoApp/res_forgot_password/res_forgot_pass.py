41c9bd5dcdcba1bdfc889078a534f88b7112a064
MrSon <sonk9d@gmail.com>
2023-02-19 08:00:50 +0700

    creat newserver loto

--- TodoApp/res_forgot_password/res_forgot_pass.py
+++ TodoApp/res_forgot_password/res_forgot_pass.py
@@ -44,9 +44,9 @@ def send_fastapi_otp_email(email, otp):
         MAIL_FROM=mail_name,
         MAIL_PORT=587,
         MAIL_SERVER="smtp.phuonghoang88.online",
-        # MAIL_TLS=True,
-        # MAIL_SSL=False,
-        # USE_CREDENTIALS=True,
+        MAIL_TLS=True,
+        MAIL_SSL=False,
+        USE_CREDENTIALS=True,
     )
     mail = FastMail(conf)
     message = MessageSchema(
